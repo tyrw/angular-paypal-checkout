69d13dac3eeb9435299bc043863e83fda57f900c
Alessandro Miliucci <lifeisfoo@gmail.com>
2016-10-29 09:26:56 +0200

    function deplaration separated from the directive binding
    see https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md#limit-1-per-file

--- paypal-checkout.js
+++ paypal-checkout.js
@@ -2,60 +2,62 @@
   /* globals paypal */
   'use strict';
 
-  angular.module('paypal-checkout', [])
-    .directive('paypalCheckout', function($timeout) {
-      return {
-        templateUrl: 'paypal-checkout.html',
-        restrict: 'EA',
-        scope: {
-          info: '=info'
-        },
-        link: function(scope) {
+  angular
+    .module('paypal-checkout', [])
+    .directive('paypalCheckout', paypalCheckout);
 
-          scope.showButton = false;
+  function paypalCheckout($timeout) {
+    return {
+      templateUrl: 'paypal-checkout.html',
+      restrict: 'EA',
+      scope: {
+        info: '=info'
+      },
+      link: function(scope) {
 
-          function showButton() {
-            scope.showButton = true;
-            scope.$apply();
-          }
+        scope.showButton = false;
 
-          function delayAndShowButton() {
-            $timeout(showButton, 1000);
-          }
-
-          function loadPaypalButton() {
-            paypal.checkout.setup(scope.info.merchantId, {
-              environment: scope.info.environment,
-              buttons: [{ container: 't1', shape: 'rect', size: 'medium' }]
-            });
-            delayAndShowButton();
-          }
-
-          scope.initPaypal = function() {
-            if (scope.showButton === false) { return; }
-            paypal.checkout.initXO();
-            return scope.info.url()
-              .then(function(url) {
-                return paypal.checkout.startFlow(url);
-              })
-              .catch(function(err) {
-                console.log('Problem with checkout flow', err);
-                return paypal.checkout.closeFlow();
-              });
-          };
-
-          if (!!window.paypalCheckoutReady) {
-            scope.showButton = true;
-          } else {
-            var s = document.createElement('script');
-            s.src = '//www.paypalobjects.com/api/checkout.js';
-            document.body.appendChild(s);
-            window.paypalCheckoutReady = function() {
-              return loadPaypalButton();
-            };
-          }
-
+        function showButton() {
+          scope.showButton = true;
+          scope.$apply();
         }
-      };
-    });
+
+        function delayAndShowButton() {
+          $timeout(showButton, 1000);
+        }
+
+        function loadPaypalButton() {
+          paypal.checkout.setup(scope.info.merchantId, {
+            environment: scope.info.environment,
+            buttons: [{ container: 't1', shape: 'rect', size: 'medium' }]
+          });
+          delayAndShowButton();
+        }
+
+        scope.initPaypal = function() {
+          if (scope.showButton === false) { return; }
+          paypal.checkout.initXO();
+          return scope.info.url()
+            .then(function(url) {
+              return paypal.checkout.startFlow(url);
+            })
+            .catch(function(err) {
+              console.log('Problem with checkout flow', err);
+              return paypal.checkout.closeFlow();
+            });
+        };
+
+        if (!!window.paypalCheckoutReady) {
+          scope.showButton = true;
+        } else {
+          var s = document.createElement('script');
+          s.src = '//www.paypalobjects.com/api/checkout.js';
+          document.body.appendChild(s);
+          window.paypalCheckoutReady = function() {
+            return loadPaypalButton();
+          };
+        }
+      }
+    }
+  }
 })();
