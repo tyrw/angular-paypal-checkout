466f10e0dc7aab3e97e95024169f07738c780edc
Tyler <tyler@eeosk.com>
2016-11-07 22:34:26 -0800

    Updated directive per Angular team style guide

--- paypal-checkout.js
+++ paypal-checkout.js
@@ -1,74 +1,80 @@
-'use strict'
-var module = angular.module('paypal-checkout', [])
+(function () {
+  'use strict'
+  angular.module('paypal-checkout', [])
 
-module.directive("paypalCheckout", function($http, $q, $timeout) {
-  return {
-    templateUrl: 'paypal-checkout.html',
-    restrict: 'EA',
-    scope: {},
-    link: function(scope, ele, attrs) {
-      var environment = 'sandbox'       // CHANGE AS NEEDED
-      var merchantId  = '6XF3MPZBZV6HU' // YOUR MERCHANT ID HERE (or import with scope)
-      var req = {
-        method: 'POST',
-        url: 'http://foo.bar',          // YOUR SERVER HERE (or import with scope)
-        data: { foo: 'bar' }            // YOUR DATA HERE (or import with scope)
-      }
-      scope.showButton = false
+  angular
+    .module('paypal-checkout')
+    .directive('paypalCheckout', paypalCheckout)
 
-      function sendRequest(data) {
-        var deferred = $q.defer()
-        $http(data)
-          .success(function(data, status) {
-            return deferred.resolve(data)
-          }).error(function(data, status) {
-            if (status === 0) { data = 'Connection error' }
-            return deferred.reject(data)
-          })
-        return deferred.promise
-      }
-
-      function showButton() {
-        scope.showButton = true
-        scope.$apply()
-      }
-
-      function delayAndShowButton() {
-        $timeout(showButton, 1000)
-      }
-
-      function loadPaypalButton() {
-        paypal.checkout.setup(merchantId, {
-          environment: environment,
-          buttons: [{ container: 't1', shape: 'rect', size: 'medium' }]
-        })
-        delayAndShowButton()
-      }
-
-      scope.initPaypal = function() {
-        if (scope.showButton == false) { return }
-        paypal.checkout.initXO()
-        return sendRequest(req)
-          .then(function(res) {
-            return paypal.checkout.startFlow(res.href)
-          })
-          .catch(function(err) {
-            console.log('Problem with checkout flow', err)
-            return paypal.checkout.closeFlow()
-          })
-      }
-
-      if (window.paypalCheckoutReady != null) {
-        scope.showButton = true
-      } else {
-        var s = document.createElement('script')
-        s.src = '//www.paypalobjects.com/api/checkout.js'
-        document.body.appendChild(s)
-        window.paypalCheckoutReady = function() {
-          return loadPaypalButton()
+  function paypalCheckout ($http, $q, $timeout) {
+    return {
+      templateUrl: 'paypal-checkout.html',
+      restrict: 'EA',
+      scope: {},
+      link: function(scope, ele, attrs) {
+        var environment = 'sandbox'       // CHANGE AS NEEDED
+        var merchantId  = '6XF3MPZBZV6HU' // YOUR MERCHANT ID HERE (or import with scope)
+        var req = {
+          method: 'POST',
+          url: 'http://foo.bar',          // YOUR SERVER HERE (or import with scope)
+          data: { foo: 'bar' }            // YOUR DATA HERE (or import with scope)
         }
-      }
+        scope.showButton = false
 
+        function sendRequest(data) {
+          var deferred = $q.defer()
+          $http(data)
+            .success(function(data, status) {
+              return deferred.resolve(data)
+            }).error(function(data, status) {
+              if (status === 0) { data = 'Connection error' }
+              return deferred.reject(data)
+            })
+          return deferred.promise
+        }
+
+        function showButton() {
+          scope.showButton = true
+          scope.$apply()
+        }
+
+        function delayAndShowButton() {
+          $timeout(showButton, 1000)
+        }
+
+        function loadPaypalButton() {
+          paypal.checkout.setup(merchantId, {
+            environment: environment,
+            buttons: [{ container: 't1', shape: 'rect', size: 'medium' }]
+          })
+          delayAndShowButton()
+        }
+
+        scope.initPaypal = function() {
+          if (scope.showButton == false) { return }
+          paypal.checkout.initXO()
+          return sendRequest(req)
+            .then(function(res) {
+              return paypal.checkout.startFlow(res.href)
+            })
+            .catch(function(err) {
+              console.log('Problem with checkout flow', err)
+              return paypal.checkout.closeFlow()
+            })
+        }
+
+        if (window.paypalCheckoutReady != null) {
+          scope.showButton = true
+        } else {
+          var s = document.createElement('script')
+          s.src = '//www.paypalobjects.com/api/checkout.js'
+          document.body.appendChild(s)
+          window.paypalCheckoutReady = function() {
+            return loadPaypalButton()
+          }
+        }
+
+      }
     }
   }
-})
+})()
